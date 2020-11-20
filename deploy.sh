1d70a9bb5996a7f9d8bd4daf6346e86c7acd8d07
jakub skory <jakub.skory@atos.net>
2020-11-20 22:01:55 +0100

    Added dirty deploy to mydevil.net

---
+++ deploy.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+rsync -Pvtr public_html/ d:/home/q84fh/domains/wkontakcie.q84fh.net/public_html/
+rsync -Pvtr public_html/ d:/home/q84fh/domains/wkontakcie.innersavages.pl/public_html/
